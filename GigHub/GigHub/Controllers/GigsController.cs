cf5b3083d4a9f2fc76f07eea54c4b084d4d778cf
tohermann <tth4ik@gmail.com>
2021-08-15 12:40:11 +0300

    added model template in Create Gig View

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using GigHub.Data;
+using GigHub.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Create()
+        {
+
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
--- GigHub/GigHub/Startup.cs
+++ GigHub/GigHub/Startup.cs
@@ -1,17 +1,11 @@
 using GigHub.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GigHub
 {
@@ -34,6 +28,8 @@ namespace GigHub
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
